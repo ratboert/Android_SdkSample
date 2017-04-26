db5b889bd18e54e28c13f296dc41fda5f33d9eaf
zhoufengan <zhoufengan@auteltech.net>
2017-04-26 16:13:50 +0800

    DSP modify

--- app/src/main/java/com/autel/sdksample/DspActivity.java
+++ app/src/main/java/com/autel/sdksample/DspActivity.java
@@ -1,24 +1,51 @@
 package com.autel.sdksample;
 
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import com.autel.common.CallbackWithNoParam;
+import com.autel.common.CallbackWithOneParam;
+import com.autel.common.dsp.AutelCancellable;
+import com.autel.common.dsp.RFData;
+import com.autel.common.error.AutelError;
+import com.autel.sdk.AModuleDsp;
+import com.autel.sdk.dsp.AutelDsp;
 import com.autel.sdksample.util.DspTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DspActivity extends AppCompatActivity {
     final String TAG = getClass().getSimpleName();
-//    RadioGroup group;
     private TextView dsp_log;
     private EditText dsp_set_rf_value;
     private EditText ssidName;
     private EditText ssidPwd;
+    private Spinner dspRFList;
+    private AutelDsp autelDsp;
 
+    private static AutelCancellable getRFList;
+    private static AutelCancellable getCurrentRF;
+    private static AutelCancellable setCurrentRF;
+
+    private RFListAdapter rfListAdapter;
+    private int selectedRFHz = -1;
 
     Handler handler = new Handler() {
         @Override
@@ -31,65 +58,161 @@ public class DspActivity extends AppCompatActivity {
     };
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dsp);
-//        group = (RadioGroup) findViewById(R.id.type);
-//        RadioButton radioGroup = (RadioButton) findViewById(R.id.async);
-//        radioGroup.toggle();
-//        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                if (R.id.async == checkedId) {
-//                    cType = 1;
-//                } else {
-//                    cType = 2;
-//                }
-//
-//            }
-//        });
+        autelDsp = AModuleDsp.dsp();
         ssidName = (EditText) findViewById(R.id.SSIDName);
         ssidPwd = (EditText) findViewById(R.id.SSIDPwd);
         dsp_log = (TextView) findViewById(R.id.dsp_log);
-        dsp_set_rf_value = (EditText) findViewById(R.id.dsp_set_rf_value);
+        dsp_log.requestFocus();
+
+        rfListAdapter = new RFListAdapter(this);
+        dspRFList = (Spinner) findViewById(R.id.dspRFList);
+        dspRFList.setAdapter(rfListAdapter);
+        dspRFList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedRFHz = ((RFData) rfListAdapter.getItem(position)).hz;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     public void setCurrentRFAStart(View v) {
-        int rf = 0;
-        if (!"".equals(dsp_set_rf_value.getText().toString())) {
-            rf = Integer.valueOf(dsp_set_rf_value.getText().toString());
+        if (-1 == selectedRFHz) {
+            logOut("setCurrentRFData  error  has not select a RF Hz");
+            return;
         }
-        DspTest.setCurrentRFStart(rf, handler);
+        setCurrentRF = autelDsp.setCurrentRFData(selectedRFHz, 3, new CallbackWithNoParam() {
+            @Override
+            public void onFailure(AutelError error) {
+                logOut("setCurrentRFData  error  " + error.getDescription());
+            }
+
+            @Override
+            public void onSuccess() {
+                logOut("setCurrentRFData  onSuccess");
+            }
+        });
     }
 
     public void setCurrentRFACancel(View v) {
-        DspTest.setCurrentRFCancel(handler);
+        if (null != setCurrentRF) {
+            setCurrentRF.cancel();
+        }
     }
 
-
     public void getCurrentRFStart(View v) {
-        DspTest.getCurrentRFStart(handler);
+        getCurrentRF = autelDsp.getCurrentRFData(3, new CallbackWithOneParam<RFData>() {
+            @Override
+            public void onFailure(AutelError error) {
+                logOut("getCurrentRFData  error  " + error.getDescription());
+            }
+
+            @Override
+            public void onSuccess(RFData data) {
+                logOut("getCurrentRFData  " + data);
+            }
+        });
     }
 
     public void getCurrentRFCancel(View v) {
-        DspTest.getCurrentRFCancel(handler);
+        if (null != getCurrentRF) {
+            getCurrentRF.cancel();
+        }
     }
 
     public void getRFListStart(View v) {
-        DspTest.getRFListStart(handler);
+        getRFList = autelDsp.getRFDataList(3, new CallbackWithOneParam<List<RFData>>() {
+            @Override
+            public void onFailure(AutelError error) {
+                logOut("getRFDataList  error  " + error.getDescription());
+            }
+
+            @Override
+            public void onSuccess(List<RFData> data) {
+                rfListAdapter.setRfData(data);
+                dspRFList.setAdapter(rfListAdapter);
+                logOut("getRFDataList  data  " + data);
+            }
+        });
     }
 
     public void getRFListCancel(View v) {
-        DspTest.getRFListCancel(handler);
+        if (null != getRFList) {
+            getRFList.cancel();
+        }
     }
 
     public void updateNewSSIDInfo(View v) {
-        DspTest.updateNewSSIDInfo(handler, ssidName.getText().toString(), ssidPwd.getText().toString());
+        autelDsp.updateNewSSIDInfo(ssidName.getText().toString(), ssidPwd.getText().toString(), new CallbackWithNoParam() {
+            @Override
+            public void onFailure(AutelError error) {
+                logOut("updateNewSSIDInfo  error  " + error.getDescription());
+            }
+
+            @Override
+            public void onSuccess() {
+                logOut("updateNewSSIDInfo  onSuccess");
+            }
+        });
     }
 
     public void getCurrentSSIDInfo(View v) {
-        DspTest.getCurrentSSIDInfo(handler);
+        logOut("getCurrentSSIDInfo  " + autelDsp.getCurrentSSIDInfo());
+    }
+
+    static class RFListAdapter extends BaseAdapter {
+        private List<RFData> rfData;
+        private Context mContext;
+
+        public RFListAdapter(Context context) {
+            mContext = context;
+        }
+
+        public void setRfData(List<RFData> rfData) {
+            this.rfData = rfData;
+            notifyDataSetInvalidated();
+        }
+
+        @Override
+        public int getCount() {
+            return null == rfData ? 0 : rfData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return rfData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (null == convertView) {
+                convertView = View.inflate(mContext, R.layout.spinner_item__rf_hz, null);
+            }
+
+            ((TextView) convertView.findViewById(R.id.rfHz)).setText("" + rfData.get(position).hz);
+
+            return convertView;
+        }
+    }
+
+    private void logOut(String log) {
+        Log.v(TAG,log);
+        Message msg = handler.obtainMessage();
+        msg.obj = log;
+        handler.sendMessage(msg);
     }
 }
