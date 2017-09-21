494a2d72d3caea906b0cdc25c94ec0ecfcdfbb0c
zhoufengan_A16343 <zhoufengan@auteltech.net>
2017-09-21 19:19:29 +0800

    ui bug fix for g2

--- app/src/main/java/com/autel/sdksample/g2/G2DspActivity.java
+++ app/src/main/java/com/autel/sdksample/g2/G2DspActivity.java
@@ -1,5 +1,10 @@
 package com.autel.sdksample.g2;
 
+import android.view.View;
+
+import com.autel.common.CallbackWithOneParam;
+import com.autel.common.dsp.g2.G2DspInfo;
+import com.autel.common.error.AutelError;
 import com.autel.sdk.dsp.AutelDsp;
 import com.autel.sdk.dsp.G2Dsp;
 import com.autel.sdk.product.BaseProduct;
@@ -28,5 +33,27 @@ public class G2DspActivity extends DspActivity {
     @Override
     protected void initUi() {
         super.initUi();
+        findViewById(R.id.setDspInfoListener).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mG2Dsp.setDspInfoListener(new CallbackWithOneParam<G2DspInfo>() {
+                    @Override
+                    public void onSuccess(G2DspInfo g2DspInfo) {
+                        logOut("setDspInfoListener onSuccess " + g2DspInfo);
+                    }
+
+                    @Override
+                    public void onFailure(AutelError autelError) {
+                        logOut("setDspInfoListener onFailure " + autelError.getDescription());
+                    }
+                });
+            }
+        });
+        findViewById(R.id.resetDspInfoListener).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mG2Dsp.setDspInfoListener(null);
+            }
+        });
     }
 }
